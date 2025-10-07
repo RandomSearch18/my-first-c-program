7c55273365c91dee23f004efc79d29cf5efe73ee
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2025-10-07 12:19:22 +0100

    Write some code from a lab session

---
+++ data_explorer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char a_char = 'A';
+    unsigned char an_unsigned = 210;
+    short a_short = 12345;
+    printf("Program is paused. Check debugger now.");
+    return 0;
+}
